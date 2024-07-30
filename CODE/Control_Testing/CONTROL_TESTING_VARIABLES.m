68a658c77b8a4006abf6fe6ef34c0c54820d357d
nyepiyapat <nyepiyapat2003@gmail.com>
2024-07-30 17:17:23 +1200

    Simulation

---
+++ CODE/Control_Testing/CONTROL_TESTING_VARIABLES.m
@@ -0,0 +1,20 @@
+clc,clear,close
+FP_Old_Max = 5;
+FP_Old_Min = 0;
+FP_New_Min_Percent = 0;
+FP_New_Max_Percent = 255;
+FP_New_Min_Angle = 0;
+FP_New_Max_Angle = 360;
+
+TB_Old_Max = 5;
+TB_Old_Min = 0;
+TB_New_Min_Percent = 0;
+TB_New_Max_Percent = 255;
+TB_New_Min_Angle = 0;
+TB_New_Max_Angle = 360;
+
+C = 2.5;
+
+
+
+
