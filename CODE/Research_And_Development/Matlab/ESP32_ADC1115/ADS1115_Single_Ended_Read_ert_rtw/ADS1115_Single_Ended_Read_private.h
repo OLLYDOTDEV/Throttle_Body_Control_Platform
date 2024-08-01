74beb52edbe99554d6257bd27f464ab582e7610c
OLLYDOTDEV <obell154@gmail.com>
2024-08-01 13:43:46 +1200

    Major rename of folder
    matlab doesnt like have spaces in names of files or folders

---
+++ CODE/Research_And_Development/Matlab/ESP32_ADC1115/ADS1115_Single_Ended_Read_ert_rtw/ADS1115_Single_Ended_Read_private.h
@@ -0,0 +1,56 @@
+/*
+ * Community and Technical College License -- for use in teaching and
+ * meeting course requirements at community and technical colleges only.
+ * Not for government, commercial, university, or other organizational
+ * use.
+ *
+ * File: ADS1115_Single_Ended_Read_private.h
+ *
+ * Code generated for Simulink model 'ADS1115_Single_Ended_Read'.
+ *
+ * Model version                  : 1.11
+ * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
+ * C/C++ source code generated on : Thu Aug  1 13:40:46 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives:
+ *    1. Traceability
+ *    2. Debugging
+ *    3. Execution efficiency
+ * Validation result: Not run
+ */
+
+#ifndef ADS1115_Single_Ended_Read_private_h_
+#define ADS1115_Single_Ended_Read_private_h_
+#include "rtwtypes.h"
+#include "ADS1115_Single_Ended_Read_types.h"
+#include "rtw_continuous.h"
+#include "rtw_solver.h"
+#ifdef __cplusplus
+#define SFB_EXTERN_C                   extern "C"
+#else
+#define SFB_EXTERN_C                   extern
+#endif
+
+SFB_EXTERN_C void ADS1115_Interface_Start_wrapper(real_T *xD);
+SFB_EXTERN_C void ADS1115_Interface_Outputs_wrapper(int16_T *A0,
+  int16_T *A2,
+  int16_T *A1,
+  int16_T *A3,
+  const real_T *xD);
+SFB_EXTERN_C void ADS1115_Interface_Update_wrapper(int16_T *A0,
+  int16_T *A2,
+  int16_T *A1,
+  int16_T *A3,
+  real_T *xD);
+SFB_EXTERN_C void ADS1115_Interface_Terminate_wrapper(real_T *xD);
+
+#undef SFB_EXTERN_C
+#endif                                /* ADS1115_Single_Ended_Read_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ CODE/Research_And_Development/Matlab/ESP32_ADC1115/ADS1115_Single_Ended_Read_ert_rtw/ert_main.c
@@ -0,0 +1,121 @@
+/*
+ * Community and Technical College License -- for use in teaching and
+ * meeting course requirements at community and technical colleges only.
+ * Not for government, commercial, university, or other organizational
+ * use.
+ *
+ * File: ert_main.c
+ *
+ * Code generated for Simulink model 'ADS1115_Single_Ended_Read'.
+ *
+ * Model version                  : 1.11
+ * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
+ * C/C++ source code generated on : Thu Aug  1 13:40:46 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives:
+ *    1. Traceability
+ *    2. Debugging
+ *    3. Execution efficiency
+ * Validation result: Not run
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ADS1115_Single_Ended_Read.h"
+#include "ADS1115_Single_Ended_Read_private.h"
+#include "rtwtypes.h"
+#include "limits.h"
+#include "MW_ArduinoHWInit.h"
+#include "mw_freertos.h"
+#define UNUSED(x)                      x = x
+#define NAMELEN                        16
+
+/* Function prototype declaration*/
+void exitFcn(int sig);
+void *terminateTask(void *arg);
+void *baseRateTask(void *arg);
+void *subrateTask(void *arg);
+volatile boolean_T stopRequested = false;
+volatile boolean_T runModel = true;
+SemaphoreHandle_t stopSem;
+SemaphoreHandle_t baserateTaskSem;
+mw_thread_t schedulerThread;
+mw_thread_t baseRateThread;
+void *threadJoinStatus;
+int terminatingmodel = 0;
+void *baseRateTask(void *arg)
+{
+  runModel = (rtmGetErrorStatus(ADS1115_Single_Ended_Read_M) == (NULL));
+  while (runModel) {
+    mw_osSemaphoreWaitEver(&baserateTaskSem);
+    ADS1115_Single_Ended_Read_step();
+
+    /* Get model outputs here */
+    stopRequested = !((rtmGetErrorStatus(ADS1115_Single_Ended_Read_M) == (NULL)));
+    runModel = !stopRequested;
+  }
+
+  runModel = 0;
+  terminateTask(arg);
+  mw_osThreadExit((void *)0);
+  return NULL;
+}
+
+void exitFcn(int sig)
+{
+  UNUSED(sig);
+  rtmSetErrorStatus(ADS1115_Single_Ended_Read_M, "stopping the model");
+}
+
+void *terminateTask(void *arg)
+{
+  UNUSED(arg);
+  terminatingmodel = 1;
+
+  {
+    runModel = 0;
+  }
+
+  /* Terminate model */
+  ADS1115_Single_Ended_Read_terminate();
+  mw_osSemaphoreRelease(&stopSem);
+  return NULL;
+}
+
+int app_main(int argc, char **argv)
+{
+  init();
+  MW_Arduino_Init();
+  rtmSetErrorStatus(ADS1115_Single_Ended_Read_M, 0);
+
+  /* Initialize model */
+  ADS1115_Single_Ended_Read_initialize();
+
+  /* Call RTOS Initialization function */
+  mw_RTOSInit(0.005, 0);
+
+  /* Wait for stop semaphore */
+  mw_osSemaphoreWaitEver(&stopSem);
+
+#if (MW_NUMBER_TIMER_DRIVEN_TASKS > 0)
+
+  {
+    int i;
+    for (i=0; i < MW_NUMBER_TIMER_DRIVEN_TASKS; i++) {
+      CHECK_STATUS(mw_osSemaphoreDelete(&timerTaskSem[i]), 0,
+                   "mw_osSemaphoreDelete");
+    }
+  }
+
+#endif
+
+  return 0;
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
