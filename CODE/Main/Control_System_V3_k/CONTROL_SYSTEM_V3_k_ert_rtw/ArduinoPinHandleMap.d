dbef31166b79c4670355006249b3bac22340700b
OLLYDOTDEV <OLLYDOTDEV@GMAIL.COM>
2024-11-11 23:06:58 +1300

    removed outdated copy's of old files

--- CODE/Main/Control_System_V3_k/CONTROL_SYSTEM_V3_k_ert_rtw/ArduinoPinHandleMap.d
+++
@@ -1,3 +0,0 @@
-ArduinoPinHandleMap.o: \
- C:/ProgramData/MATLAB/SupportPackages/R2024a/toolbox/target/supportpackages/arduinobase/src/ArduinoPinHandleMap.cpp \
- E:/Banana444/Documents/GitHub/Throttle_Body_Control_Platform/CODE/Research_And_Development/Matlab/Control_System_V3_k/CONTROL_SYSTEM_V3_k_ert_rtw/rtwtypes.h
--- CODE/Main/Control_System_V3_k/CONTROL_SYSTEM_V3_k_ert_rtw/rtGetNaN.d
+++
@@ -1,5 +0,0 @@
-rtGetNaN.o: \
- E:/Banana444/Documents/GitHub/Throttle_Body_Control_Platform/CODE/Research_And_Development/Matlab/Control_System_V3_k/CONTROL_SYSTEM_V3_k_ert_rtw/rtGetNaN.c \
- E:/Banana444/Documents/GitHub/Throttle_Body_Control_Platform/CODE/Research_And_Development/Matlab/Control_System_V3_k/CONTROL_SYSTEM_V3_k_ert_rtw/rtwtypes.h \
- E:/Banana444/Documents/GitHub/Throttle_Body_Control_Platform/CODE/Research_And_Development/Matlab/Control_System_V3_k/CONTROL_SYSTEM_V3_k_ert_rtw/rtGetNaN.h \
- E:/Banana444/Documents/GitHub/Throttle_Body_Control_Platform/CODE/Research_And_Development/Matlab/Control_System_V3_k/CONTROL_SYSTEM_V3_k_ert_rtw/rt_nonfinite.h
